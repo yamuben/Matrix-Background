0070d27da4f268b9497209ed5015b7ca0cb5a86a
iyamuremye Benjamin <yamubbenjamin@gmail.com>
2022-04-30 16:55:52 +0200

    1st change

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const canvas = document.getElementById('Matrix');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("Matrix");
+const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
+const katakana =
+  "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const nums = "0X";
 
-const alphabet = katakana + latin + nums;
+const alphabet = nums;
 
-const fontSize = 16;
-const columns = canvas.width/fontSize;
+let fontSize = 15; //1600
+const columns = canvas.width / fontSize;
 
 const rainDrops = [];
 
-for( let x = 0; x < columns; x++ ) {
-	rainDrops[x] = 1;
+for (let x = 0; x < columns; x++) {
+  rainDrops[x] = 0;
 }
 
 const draw = () => {
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	
-	context.fillStyle = '#0F0';
-	context.font = fontSize + 'px monospace';
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-	for(let i = 0; i < rainDrops.length; i++)
-	{
-		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
-		
-		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
-			rainDrops[i] = 0;
-        }
-		rainDrops[i]++;
-	}
+  for (let i = 0; i < rainDrops.length; i++) {
+    context.fillStyle = `rgba(0, 255, 0, ${Math.random()})`;
+    context.font = Math.floor(Math.random() * fontSize) + "px monospace";
+
+    const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    context.fillText(text, rainDrops[i] * fontSize, i * fontSize);
+
+    if (rainDrops[i] * fontSize > canvas.width && Math.random() > 0.575) {
+      rainDrops[i] = 0;
+      fontSize = Math.floor(Math.random() * fontSize) + rainDrops.length / 2;
+    }
+
+    rainDrops[i]++;
+    // console.log(":::index:::",i, "==",rainDrops[i]);
+  }
+  // console.log("###",rainDrops )
 };
-
-setInterval(draw, 30);
+// draw()
+// draw()
+setInterval(draw, 100);
