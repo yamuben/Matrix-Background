893b67ea7b3c6cffa643d641504e2e8dc360c052
iyamuremye Benjamin <yamubbenjamin@gmail.com>
2022-05-01 07:36:02 +0200

    updates

--- index.js
+++ index.js
@@ -3,61 +3,126 @@ const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-const katakana =
-  "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
-const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const nums = "01";
 
 const alphabet = nums;
 
-let fontSize = 15; //1600
+const fontSize = 10;
 const columns = canvas.width / fontSize;
 
 const rainDrops = [];
 
 for (let x = 0; x < columns; x++) {
-  rainDrops[x] = 0;
+  rainDrops[x] = 1;
 }
-let sample = 1;
 
 const draw = () => {
   context.fillStyle = "rgba(0, 0, 0, 0.05)";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
+  context.fillStyle = "#0F0";
+  context.font = fontSize + "px monospace";
+
   for (let i = 0; i < rainDrops.length; i++) {
-    context.fillStyle = `rgba(0, 255, 0, ${Math.random() / 2})`;
-    context.font = Math.floor(Math.random() * fontSize) + "px monospace";
     const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-    for (let j = 0; j <= i; j++) {
-      context.fillText(
-        text,
-        i * fontSize,
-        rainDrops[i] * fontSize > canvas.width
-          ? (rainDrops[i] * fontSize) / 2
-          : rainDrops[i] * fontSize
-      );
-    }
+    context.fillText(text, rainDrops[i] * fontSize, i * fontSize);
 
-    if (rainDrops[i] * fontSize > canvas.width && Math.random() > 0.575) {
+    if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
       rainDrops[i] = 0;
-      fontSize = Math.floor(Math.random() * fontSize) + rainDrops.length;
     }
-    sample++;
-    if (sample % i === 0) {
-      fontSize = Math.floor(Math.random() * fontSize) + rainDrops.length / i;
-    }
-    if (sample % 2 === 0) {
-      fontSize = Math.floor(Math.random() * fontSize) + rainDrops.length / 2;
-    }
-    if (sample % 3 === 0) {
-      fontSize = 20;
-    }
-
     rainDrops[i]++;
-    // console.log(":::index:::",i, "==",rainDrops[i]);
   }
 };
 
-// draw()
 setInterval(draw, 100);
+
+//................................................................
+
+
+const fontSize20 = 25;
+const columns20 = canvas.width / fontSize20;
+
+const rainDrops20 = [];
+
+for (let x = 0; x < columns20; x++) {
+  rainDrops20[x] = 1;
+}
+
+const draw20 = () => {
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "#0F0";
+  context.font = fontSize20 + "px monospace";
+
+  for (let i = 0; i < rainDrops20.length; i++) {
+    const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    context.fillText(text, rainDrops20[i] * fontSize20, i * fontSize20);
+
+    if (rainDrops20[i] * fontSize20 > canvas.height && Math.random() > 0.975) {
+      rainDrops20[i] = 0;
+    }
+    rainDrops20[i]++;
+  }
+};
+setInterval(draw20, 450);
+//................................................................
+
+
+const fontSize50 = 70;
+const columns50 = canvas.width / fontSize50;
+
+const rainDrops50 = [];
+
+for (let x = 0; x < columns50; x++) {
+  rainDrops50[x] = 1;
+}
+
+const draw50 = () => {
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "#0F0";
+  context.font = fontSize50 + "px monospace";
+
+  for (let i = 0; i < rainDrops50.length; i++) {
+    const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    context.fillText(text, rainDrops50[i] * fontSize50, i * fontSize50);
+
+    if (rainDrops50[i] * fontSize50 > canvas.height && Math.random() > 0.975) {
+      rainDrops50[i] = 0;
+    }
+    rainDrops50[i]++;
+  }
+};
+setInterval(draw50, 700);
+//................................................................
+
+
+const fontSize35 = 35;
+const columns35 = canvas.width / fontSize35;
+
+const rainDrops35 = [];
+
+for (let x = 0; x < columns35; x++) {
+  rainDrops35[x] = 1;
+}
+
+const draw35 = () => {
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "#0F0";
+  context.font = fontSize35 + "px monospace";
+
+  for (let i = 0; i < rainDrops35.length; i++) {
+    const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    context.fillText(text, rainDrops35[i] * fontSize35, i * fontSize35);
+
+    if (rainDrops35[i] * fontSize35 > canvas.height && Math.random() > 0.975) {
+      rainDrops35[i] = 0;
+    }
+    rainDrops35[i]++;
+  }
+};
+setInterval(draw35, 450);
